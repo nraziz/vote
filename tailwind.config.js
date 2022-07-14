5690a5e3b50de9ff5e289fa412fbb5d63cdba3bb
Aziz Khan <nr.aziz@gmail.com>
2022-07-14 12:06:10 +0100

    Installed more packages and moved to src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "git",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
     extend: {
       container: {
